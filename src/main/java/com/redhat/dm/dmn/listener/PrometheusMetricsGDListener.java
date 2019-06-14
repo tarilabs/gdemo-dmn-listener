8eaac7ca9ae5a11b3247b69b1f243ca86f1538d1
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-06-14 13:18:52 +0200

    Added required metrics. Added registration check for Prometheus Collectors.

--- src/main/java/com/redhat/dm/dmn/listener/PrometheusMetricsGDListener.java
+++ src/main/java/com/redhat/dm/dmn/listener/PrometheusMetricsGDListener.java
@@ -15,8 +15,10 @@
 package com.redhat.dm.dmn.listener;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-import io.prometheus.client.Histogram;
+import org.jbpm.services.api.service.ServiceRegistry;
 import org.kie.dmn.api.core.DMNDecisionResult;
 import org.kie.dmn.api.core.ast.DecisionNode;
 import org.kie.dmn.api.core.event.AfterEvaluateBKMEvent;
@@ -30,22 +32,79 @@ import org.kie.dmn.api.core.event.BeforeEvaluateDecisionEvent;
 import org.kie.dmn.api.core.event.BeforeEvaluateDecisionServiceEvent;
 import org.kie.dmn.api.core.event.BeforeEvaluateDecisionTableEvent;
 import org.kie.dmn.api.core.event.DMNRuntimeEventListener;
-import org.kie.server.api.model.ReleaseId;
-import org.kie.server.services.api.KieContainerInstance;
+
+import io.prometheus.client.Histogram;
 
 public class PrometheusMetricsGDListener implements DMNRuntimeEventListener {
 
-    private static final Histogram riskEvaluations = Histogram.build()
-            .name("risk_score")
-            .help("Risk Score")
-            .labelNames("container_id", "group_id", "artifact_id", "version", "decision_namespace", "decision_name")
-            .buckets(10, 20, 30, 40, 50, 60 , 70, 80, 90)
-            .register();
+    private static final String CARD_HOLDER_RISK_RATING_DECISION = "Cardholder Risk Rating";
+    private static final String DISPUTE_RISK_RATING_DECISION = "Dispute Risk Rating";
+    private static final String PROCESS_AUTOMATICALLY_DECISION = "Process Automatically";
 
-    private final KieContainerInstance kieContainer;
+    private static final String CARD_HOLDER_RISK_RATING_COLLECTOR = "CardHolderRisktRatingCollector";
+    private static final String DISPUTE_RISK_RATING_COLLECTOR = "DisputeRiskRatingCollector";
+    private static final String PROCESS_AUTOMATICALLY_RATING_COLLECTOR = "DisputeRiskRatingCollector";
 
-    public PrometheusMetricsGDListener(KieContainerInstance kieContainer) {
-        this.kieContainer = kieContainer;
+    private static final Histogram disputeRiskRating;
+
+    private static final Histogram cardholderRiskRating;
+
+    private static final Histogram processAutomaticallyRating;
+
+    private final String[] labels;
+
+    static {
+        ServiceRegistry registry = ServiceRegistry.get();
+        
+        Histogram crr = null;
+        try {
+            crr = (Histogram) registry.service(CARD_HOLDER_RISK_RATING_COLLECTOR);
+        } catch (IllegalArgumentException iae) {
+            //Collector is not in registry, so building a new one.
+            crr = Histogram.build()
+                    .name("cardholder_risk_rating")
+                    .help("Cardholder Risk Rating")
+                    .labelNames("group_id", "artifact_id", "version", "decision_namespace", "decision_name")
+                    .buckets(1, 2, 3, 4, 5)
+                    .register();
+
+            registry.register(CARD_HOLDER_RISK_RATING_COLLECTOR, crr);
+        }
+        cardholderRiskRating = crr;
+
+        Histogram drr = null;
+        try {
+            drr = (Histogram) registry.service(DISPUTE_RISK_RATING_COLLECTOR);
+        } catch (IllegalArgumentException iae) {
+            //Collector is not in registry, so building a new one.
+            drr = Histogram.build()
+                    .name("dispute_risk_rating")
+                    .help("Dispute Risk Rating")
+                    .labelNames("group_id", "artifact_id", "version", "decision_namespace", "decision_name")
+                    .buckets(1, 2, 3, 4, 5)
+                    .register();
+            registry.register(DISPUTE_RISK_RATING_COLLECTOR, drr);
+        }
+        disputeRiskRating = drr;
+    
+        Histogram par = null;
+        try {
+            par = (Histogram) registry.service(PROCESS_AUTOMATICALLY_RATING_COLLECTOR);
+        } catch (IllegalArgumentException iae) {
+            //Collector is not in registry, so building a new one.
+            par = Histogram.build()
+                    .name("process_automatically_risk_rating")
+                    .help("Process Automatically Risk Rating")
+                    .labelNames("group_id", "artifact_id", "version", "decision_namespace", "decision_name")
+                    .buckets(0, 1)
+                    .register();
+            registry.register(DISPUTE_RISK_RATING_COLLECTOR, par);
+        } 
+        processAutomaticallyRating = par;
+    }
+
+    public PrometheusMetricsGDListener(String... labels) {
+        this.labels = labels;
     }
 
     @Override
@@ -55,23 +114,51 @@ public class PrometheusMetricsGDListener implements DMNRuntimeEventListener {
     @Override
     public void afterEvaluateDecision(AfterEvaluateDecisionEvent e) {
         DecisionNode decisionNode = e.getDecision();
-        ReleaseId releaseId = kieContainer.getResource().getReleaseId();
-        DMNDecisionResult decisionResultById = e.getResult().getDecisionResultById(decisionNode.getId());
-        if(decisionResultById != null && !decisionResultById.hasErrors()) {
-            BigDecimal score = (BigDecimal) e.getResult().getDecisionResultByName("risk score").getResult();
-            getRiskEvaluations().labels(kieContainer.getContainerId(),
-                                        releaseId.getGroupId(),
-                                        releaseId.getArtifactId(),
-                                        releaseId.getVersion(),
-                                        decisionNode.getModelName(),
-                                        decisionNode.getModelNamespace()).observe(score.doubleValue());
+        DMNDecisionResult disputeRiskRating = e.getResult().getDecisionResultByName(DISPUTE_RISK_RATING_DECISION);
+        DMNDecisionResult cardholderRiskRating = e.getResult().getDecisionResultByName(CARD_HOLDER_RISK_RATING_DECISION);
+        DMNDecisionResult processAutomatically = e.getResult().getDecisionResultByName(PROCESS_AUTOMATICALLY_DECISION);
+        publishToPrometheus(getDisputeRiskRatingHistogram(), decisionNode, disputeRiskRating);
+        publishToPrometheus(getCardholderRiskRatingHistogram(), decisionNode, cardholderRiskRating);
+        publishToPrometheus(getProcessAutomaticallyRating(), decisionNode, processAutomatically);
+    }
 
+    void publishToPrometheus(Histogram histogram, DecisionNode decisionNode, DMNDecisionResult dmnDecisionResult) {
+        if (dmnDecisionResult != null && !dmnDecisionResult.hasErrors()) {
+            double result = 0;
+            Object dmnResult = dmnDecisionResult.getResult();
+            if (dmnResult instanceof BigDecimal) {
+                BigDecimal bdScore = (BigDecimal) dmnDecisionResult.getResult();
+                result = bdScore.doubleValue();
+            } else if (dmnResult instanceof Boolean) {
+                Boolean bScore = (Boolean) dmnDecisionResult.getResult();
+                result = bScore.booleanValue() ? 1 : 0;
+            }
+
+            histogram.labels(buildLabels(decisionNode.getModelName(), decisionNode.getModelNamespace())).observe(result);
         }
     }
 
-    Histogram getRiskEvaluations() {
-        return riskEvaluations;
+    Histogram getDisputeRiskRatingHistogram() {
+        return disputeRiskRating;
     }
+
+    Histogram getCardholderRiskRatingHistogram() {
+        return cardholderRiskRating;
+    }
+
+    Histogram getProcessAutomaticallyRating() {
+        return processAutomaticallyRating;
+    }
+    private String[] buildLabels(String... dynamicLabels) {
+        List<String> labelsList = new ArrayList<>();
+        for (String nextStaticLabel: labels) {
+            labelsList.add(nextStaticLabel);
+        }
+        for (String nextDynamicLabel: dynamicLabels) {
+            labelsList.add(nextDynamicLabel);
+        }
+       return labelsList.toArray(new String[labelsList.size()]);
+     }
 
     @Override
     public void beforeEvaluateBKM(BeforeEvaluateBKMEvent event) {
